2bce13c26bc30e29382c332756ffe4c9aacab270
Thomas Lippert <thomas.lippert@ges.thm.de>
2017-05-17 13:11:47 +0200

    add Temp

---
+++ src/SensorTemp.py
@@ -0,0 +1,39 @@
+'''
+Created on 17.05.2017
+
+@author: Effi
+'''
+
+import thingspeak
+import time
+import Adafruit_DHT
+class SensorTemp(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, channelID, writeKey, readKey):
+        '''
+        Constructor
+        '''
+        self.channelID = channelID
+        self.writeKey = writeKey
+        self.readKey = readKey
+        self.sensor = Adafruit_DHT.DHT22
+        self.channel = thingspeak.Channel(id=channelID,write_key=writeKey,api_key=readKey)
+    def measure(channel):
+        try:
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+            # write
+            response = channel.update({'field1': temperature, 'field2': humidity})
+            
+            return temperature, humidity
+            
+            #print 'Temp: ' + str(temperature + '\nFeuchtigkeit: ' + str(humidity)) 
+        except:
+            print("connection failed")
+            return None
+            
+    def update(self):
+        self.measure(self.channel)
